a1309a8786b971ab9107c229e10cbd033fcb4e74
PrashantBhagwani123 <p.bhagwani@zensar.com>
2022-06-22 22:59:14 +0530

    sql file updated again2

--- loanbanking.sql
+++ loanbanking.sql
@@ -1,5 +1,5 @@
 use banking;
 
-INSERT INTO `customer_detail` VALUES (1,'Prashant Bhagwani','8989246159'),(2,'Divyesh Bhagwani','9425426042');
+INSERT INTO `customer_detail` VALUES (1,'Prashant Bhagwani','8989246159'),(2,'Divyesh Bhagwani','9425426042'),(3,'Mahesh Patil','9422194151');
 INSERT INTO `loan` VALUES (5025,'Home Loan'),(5030,'Vehical Loan'),(5035,'Personal Loan'),(5040,'Education Loan');
 
